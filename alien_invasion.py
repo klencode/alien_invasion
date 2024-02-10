da72cc21b62c977db2d6e5a55d23b89cab8d170f
Klenam <ayokwashie42@gmail.com>
2024-02-10 16:03:22 +0000

    modified alien_invasion and added new file - ship.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,11 +8,17 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.int()
+        
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        
         # Set the background color.
-        self.bg_color = (230, 230, 230)
+        # self.bg_color = (230, 230, 230)
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        # self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -24,10 +30,12 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
-            
+            # self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
